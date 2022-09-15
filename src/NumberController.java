c53bf90a28f39d00af128d8fc6f4fbd064ffa607
gtom06 <56637690+gtom06@users.noreply.github.com>
2022-09-15 15:17:52 +0200

    delete

--- src/NumberController.java
+++
@@ -1,8 +0,0 @@
-public class NumberController {
-    public static boolean isDivisibleBy2(double number) {
-        if (number % 2 == 0) {
-            return true;
-        }
-        return false;
-    }
-}
